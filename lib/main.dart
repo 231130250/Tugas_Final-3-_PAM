1d2d255438e44423842616e89a08f46f4e163f3a
Agus Kristanto Matondang <agus.kristanto@students.mikroskil.ac.id>
2025-07-08 00:12:04 +0700

    melakukan konfigurasi dan inisialisasi Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:wallet/firebase_options.dart';
 import 'package:wallet/providers/waallet_providers.dart';
 import 'package:wallet/screens/login_screen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   // Inisialisasi format lokal (untuk tanggal Indonesia)
   initializeDateFormatting('id_ID', null).then((_) {
     runApp(const MyApp());
